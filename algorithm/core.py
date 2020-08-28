69df9f79abd5ecd079762db3102caaba26c0dffb
Li Zhiyong <lizhiyong2000@gmail.com>
2020-08-28 17:27:30 +0800

    添加从百度下载车牌图片

--- algorithm/core.py
+++ algorithm/core.py
@@ -37,7 +37,7 @@ def tiltCorrection(vertices, rect):
         new_left_point_x = vertices[1, 0]
         new_left_point_y = int(vertices[0, 1] + (vertices[0, 0] - vertices[1, 0]) / (vertices[0, 0] - vertices[2, 0]) * (vertices[2, 1] - vertices[0, 1]))
         # # 校正后的四个顶点坐标
-        point_set_1 = np.float32([[440, 0],[0, 0],[0, 140],[440, 140]])
+        point_set_1 = np.float32([[240, 0],[0, 0],[0, 80],[240, 80]])
     # 畸变情况2
     elif rect[2] < -45:
         new_right_point_x = vertices[1, 0]
@@ -45,7 +45,7 @@ def tiltCorrection(vertices, rect):
         new_left_point_x = vertices[0, 0]
         new_left_point_y = int(vertices[1, 1] - (vertices[1, 0] - vertices[0, 0]) / (vertices[1, 0] - vertices[2, 0]) * (vertices[1, 1] - vertices[2, 1]))
         # # 校正后的四个顶点坐标
-        point_set_1 = np.float32([[0, 0],[0, 140],[440, 140],[440, 0]])
+        point_set_1 = np.float32([[0, 0],[0, 80],[240, 80],[240, 0]])
 
         # 校正前平行四边形四个顶点坐标
     new_box = np.array([(vertices[0, 0], vertices[0, 1]), (new_left_point_x, new_left_point_y), (vertices[1, 0], vertices[1, 1]), (new_right_point_x, new_right_point_y)])
@@ -92,7 +92,7 @@ def locate_and_correct(img_src, img_mask):
             approx = cv2.approxPolyDP(cont, epsilon, True)
 
 
-            cv2.drawContours(img_mask, approx, -1, (0, 0, 255), 3)
+            # cv2.drawContours(img_mask, approx, -1, (0, 0, 255), 3)
             # cv2.imshow('img_mask',img_mask)
             # cv2.waitKey(0)
 
@@ -113,7 +113,7 @@ def locate_and_correct(img_src, img_mask):
                 # 获取最可疑区域轮廓点集
                 # points = np.array(cont[:, 0])
                 #
-                # vertices, rect = findVertices(cont)
+                vertices, rect = findVertices(approx)
                 #
                 # cv2.drawContours(img_mask, vertices, 0, (0, 255, 0), 2)
                 # cv2.imshow('img_mask',img_mask)
@@ -122,7 +122,7 @@ def locate_and_correct(img_src, img_mask):
                 # point_set_0, new_box = tiltCorrection(vertices, rect)
 
 
-                rect = cv2.minAreaRect(cont)  # 针对坐标点获取带方向角的最小外接矩形，中心点坐标，宽高，旋转角度
+                # rect = cv2.minAreaRect(cont)  # 针对坐标点获取带方向角的最小外接矩形，中心点坐标，宽高，旋转角度
                 box = cv2.boxPoints(rect).astype(np.int32)  # 获取最小外接矩形四个顶点坐标
                 # # cv2.drawContours(img_mask, contours, -1, (0, 0, 255), 2)
                 # # cv2.drawContours(img_mask, [box], 0, (0, 255, 0), 2)
@@ -138,7 +138,7 @@ def locate_and_correct(img_src, img_mask):
                 box_left = sorted(box_left, key=lambda x: x[1])  # 再按照上下即y进行排序，此时box_left中为左上和左下两个端点坐标
                 box_right = sorted(box_right, key=lambda x: x[1])  # 此时box_right中为右上和右下两个端点坐标
                 box = np.array(box_left + box_right)  # [左上，左下，右上，右下]
-                # print(box)
+            #     # print(box)
                 x0, y0 = box[0][0], box[0][1]  # 这里的4个坐标即为最小外接矩形的四个坐标，接下来需获取平行(或不规则)四边形的坐标
                 x1, y1 = box[1][0], box[1][1]
                 x2, y2 = box[2][0], box[2][1]
@@ -187,20 +187,25 @@ def locate_and_correct(img_src, img_mask):
                 # 左上角，右下角, 左下角，右上角，
                 # l0, l1, l2, l3 = point_set_0[0], point_set_0[1], point_set_0[2], point_set_0[3]
                 # l0, l1, l2, l3 = approx[0], approx[1], approx[2], approx[3]
-                print([l0,l1,l2,l3])
-                for l in [l0, l1, l2, l3]:
-                    cv2.circle(img=img_mask, color=(255, 0, 0), center=tuple(l), thickness=2, radius=5)
-                cv2.imshow('img_mask',img_mask)
+                # print([l0,l1,l2,l3])
+                # for l in [l0, l1, l2, l3]:
+                #     cv2.circle(img=img_mask, color=(255, 0, 0), center=tuple(l), thickness=2, radius=5)
+                # cv2.imshow('img_mask',img_mask)
 
-                p0, p1, _ = tiltCorrection([l0,l1,l2,l3], rect)
+                p0, p1, _ = tiltCorrection(vertices, rect)
+
+
 
                 # # cv2.waitKey(0)
                 # p0 = np.float32([l0, l1, l3, l2])  #，p0和p1中的坐标顺序对应，以进行转换矩阵的形成
                 # p1 = np.float32([(0, 0), (0, 80), (240, 80), (240, 0)])  # 我们所需的长方形
                 transform_mat = cv2.getPerspectiveTransform(p0, p1)  # 构成转换矩阵
-                lic = cv2.warpPerspective(img_src, transform_mat, (440, 140))  # 进行车牌矫正
+                lic = cv2.warpPerspective(img_src, transform_mat, (240, 80))  # 进行车牌矫正
                 # cv2.imshow('lic',lic)
                 # cv2.waitKey(0)
                 Lic_img.append(lic)
                 cv2.drawContours(img_src_copy, [np.array([l0, l1, l3, l2])], -1, (0, 255, 0), 2)  # 在img_src_copy上绘制出定位的车牌轮廓，(0, 255, 0)表示绘制线条为绿色
+                cv2.drawContours(img_src_copy, p0, -1, (255, 0, 0), 2)
+                cv2.imshow('img_src_copy',img_src_copy)
+                cv2.waitKey(0)
     return img_src_copy, Lic_img
